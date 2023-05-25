58ff290a455e3bfe06cfe64a1df83bfbe6122fde
DESKTOP-JGUHHCD\Swathi <swathi.t@imaginnovate.com>
2023-05-25 11:37:18 +0530

    Added list class

--- src/main/java/org/example/collections/List.java
+++ src/main/java/org/example/collections/List.java
@@ -9,21 +9,19 @@ public class List {
         obj.add("hello");
         obj.add("Good Afternoon");
         System.out.println(obj);
-
         obj.add(1, "person");
         System.out.println(obj);
 
-        int number= obj.size();
+        int number = obj.size();
         System.out.println(number);
 
-        obj.set(1,"bye");
+        obj.set(1, "bye");
         System.out.println(obj);
 
-        String ret =obj.get(3);
+        String ret = obj.get(3);
         System.out.println(ret);
 
         obj.remove("bye");
         System.out.println(obj);
-
     }
 }
